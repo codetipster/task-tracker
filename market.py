9324812fb613515436303c002b73a7fd0e6d9fc1
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-04-19 21:11:08 +0000

    templates
    templates plus bootstrap rendering

--- market.py
+++ market.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "<h1>Are you not amazing darling flask?</h1>"
+# @app.route('/')
+# def hello_world():
+#     return "<h1>Are you not amazing darling flask?</h1>"
 
 # LESSONS ON DYNAMIC RENDERING: Think of how FB will have to hard code route functions for individual profiles 
 # @app.route('/About/<username>')
 # def about_us(username):
 #     return f'<h1>We are great devs for the future, this is {username}</h1>'    
+
+#TEMPLATES RENDERING
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
