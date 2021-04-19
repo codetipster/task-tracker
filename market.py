9ed1b072647575c6f4dc1941be24ac1ea9dd720b
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-04-19 20:50:07 +0000

    simple lesson changes

--- market.py
+++ market.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "<h1>Are you not amazing darling flask?</h1>"
+
+# LESSONS ON DYNAMIC RENDERING: Think of how FB will have to hard code route functions for individual profiles 
+# @app.route('/About/<username>')
+# def about_us(username):
+#     return f'<h1>We are great devs for the future, this is {username}</h1>'    
